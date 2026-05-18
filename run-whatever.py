b11408587ece7c1a14e9bffd4496aadad9b41da0
Igor Britsky <igor.britsky@lge.com>
2026-05-18 13:14:56 +0300

    [ci][build] Handle exception
    Issues: NEVA-11182

--- run-whatever.py
+++ run-whatever.py
@@ -4,14 +4,23 @@ import sys
 
 from relay import Job
 
-jobname, _ = os.path.relpath(sys.argv[0], ".").split(os.path.extsep)
+try:
+  jobname, _ = os.path.relpath(sys.argv[0], ".").split(os.path.extsep)
+except ValueError:
+  print("Invalid job name")
+  sys.exit(-1)
 
-q = Job(f"NEVA/{jobname}").trigger()
-print(f"Started {q.build.name}...")
+try:
+  q = Job(f"NEVA/{jobname}").trigger()
+  print(f"Started {q.build.name}...")
+except Exception as e:
+  print(f"Job {jobname} trigger failed.")
+  sys.exit(-1)
 
 status = q.join()
 print(f"...completed with {status}")
 if status != "SUCCESS":
+  print(f"Job join failed with status: {status}")
   sys.exit(-1)
 
 sys.exit(0)
